5fb000b47df97044bdbbcbf3428e68d998da6768
sayakanoa <reiluv75418@gmail.com>
2021-10-18 10:17:59 +0000

    admin/customerコントローラー追加

---
+++ app/controllers/admin/customer_controller.rb
@@ -0,0 +1,2 @@
+class Admin::CustomerController < ApplicationController
+end
--- test/controllers/admin/customer_controller_test.rb
+++ test/controllers/admin/customer_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Public::CustomerControllerTest < ActionDispatch::IntegrationTest
+class Admin::CustomerControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
